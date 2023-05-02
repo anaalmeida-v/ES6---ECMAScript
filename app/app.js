93d92855a1f74b27f24d88ad04fb100fd5d1203b
Ana <aninhaalmeida@gmail.br>
2023-05-02 17:27:04 -0300

    app 1 - criação, inclusão e teste do arquivo.js

---
+++ app/app.js
@@ -0,0 +1,12 @@
+function cadastrarDespesa() {
+
+    let ano = document.getElementById('ano')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+    //variáveis utilizadas para a referencia das seleções, para que os atributos possam ser utilizados ou métodos desses elementos
+
+    
+}
