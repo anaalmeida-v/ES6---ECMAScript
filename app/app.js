7f0844d864067a2d111398aea673b9269f3b6d36
Ana <aninhaalmeida@gmail.br>
2023-05-03 17:13:13 -0300

    criação-cadastro Despesa/WebStorage/criacaoBd-JSON

--- app/app.js
+++ app/app.js
@@ -1,3 +1,41 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+class Bd {
+
+	constructor() {
+		let id = localStorage.getItem('id')
+
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')//getItem() - recuperar um dado dentro de localStorage
+        return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+        //localStorage.setItem('despesa', JSON.stringify(d))
+        let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
 
     let ano = document.getElementById('ano')
@@ -8,5 +46,22 @@ function cadastrarDespesa() {
     let valor = document.getElementById('valor')
     //variáveis utilizadas para a referencia das seleções, para que os atributos possam ser utilizados ou métodos desses elementos
 
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
+    )
     
+    bd.gravar(despesa)
+
 }
+
+
+/*function gravar(d) {//transformando objeto literal em JSON
+    localStorage.setItem('despesa', JSON.stringify(d))/*setItem()retorna 2 parâmetros, um nome e um valor
+        quando passado 'chave' e 'valor', irá adicionar esta chave ao storage, ou atualizar o valor caso a chave já
+        exista. ESSE MÉTODO GUARDA APENAS A INFORMAÇÃO DO ÚLTIMO REGISTRO CRIADO
+}*/
